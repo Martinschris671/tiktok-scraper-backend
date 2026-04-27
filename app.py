e99054c700326291bee1482e21a2e7e7932d7a04
Martinschris671 <martinschris671@gmail.com>
2026-04-27 13:35:10 -0700

    lkjkljkla

--- app.py
+++ app.py
@@ -23,12 +23,11 @@ def get_user_info(identifier, by_id=False):
             identifier = identifier[1:]
         url = f"https://www.tiktok.com/@{identifier}"
 
-    # === NEW HEADERS IMPLEMENTED HERE ===
-    headers = {
+ headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Encoding': 'gzip, deflate', # <--- WE REMOVED ', br' FROM HERE
         'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
         'Sec-Fetch-Dest': 'document',
